e4b036b6aaa42a285b9cf69b0ab22c46b2b9fb17
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-06 14:38:22 -0400

    add city search javascript

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const searchUrl =
+  "https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=3&language=en&format=json";
+
 const weatherUrl =
   "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m";
 
@@ -52,3 +55,19 @@ function processWeatherData(data) {
     Precipitation: ${precipitation} ${precipitationUnits}
     `);
 }
+
+async function fetchDataFromAPI(url) {
+  try {
+    let response = await fetch(url, { mode: "cors" });
+    let json = await response.json();
+    console.log(json);
+    json.results.forEach((result) => {
+      console.log(`${result.name}, ${result.country}`);
+    });
+  } catch (err) {
+    console.log(`error fetching from  ${url}`);
+    console.log(err);
+  }
+}
+
+fetchDataFromAPI(searchUrl);
