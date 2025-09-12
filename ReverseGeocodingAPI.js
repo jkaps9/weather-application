c138030cb0aebf85240da7073233b17af79eef7e
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-12 00:17:37 -0400

    add ReverseGeocodingAPI

--- ReverseGeocodingAPI.js
+++ ReverseGeocodingAPI.js
@@ -1,6 +1,12 @@
 export class ReverseGeocodingAPI {
   constructor() {
-    this.baseURL =
-      "https://api.geoapify.com/v1/geocode/reverse?lat=51.21709661403662&lon=6.7782883744862374&apiKey=3b5c3fd5c58b4980a3a94c12d0f74c6c";
+    this.baseURL = "https://api.geoapify.com/v1/geocode/reverse";
+  }
+
+  async getLocation(lat, lon) {
+    fetch(`${this.baseURL}?lat=${lat}&lon=${lon}&apiKey=${this.API_KEY}`)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   }
 }
