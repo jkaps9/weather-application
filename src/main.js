17d5a61a41da8e9ae22128c374c5732679d63274
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-22 15:03:43 -0400

    remove unused js

--- src/main.js
+++ src/main.js
@@ -1,34 +1,4 @@
 import "./styles.css";
-import { ReverseGeocodingAPI } from "./assets/scripts/ReverseGeocodingAPI.js";
 import { WeatherController } from "./assets/scripts/WeatherController.js";
 
 const weatherController = new WeatherController();
-
-//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \\
-// //// BELOW IS FOR GETTING THE USERS LOCATION \\\\ \\
-
-// const apiKey = import.meta.env.VITE_API_KEY;
-// const dbHost = import.meta.env.VITE_DB_HOST;
-// const reverseGeocodingAPI = new ReverseGeocodingAPI(apiKey);
-// getUserLocation();
-
-// function getUserLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     console.log("Geolocation is not supported by this browser.");
-//   }
-// }
-
-// async function showPosition(position) {
-//   const location = await reverseGeocodingAPI.getLocation(
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-
-//   if (location === undefined) {
-//     console.log("location not found...");
-//   } else {
-//     searchCity(location.city, location.country_code.toUpperCase());
-//   }
-// }
