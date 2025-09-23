ce51ad71e910680a6346f0db6291587f7efaec97
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-23 15:12:13 -0400

    add check if location already saved

--- src/assets/scripts/LocalStorage.js
+++ src/assets/scripts/LocalStorage.js
@@ -8,18 +8,23 @@ export class LocalStorage {
   }
 
   saveLocation(location) {
-    const newLoc = {
-      name: location.name,
-      admin1: location.admin1,
-      country: location.country,
-      latitude: location.latitude,
-      longitude: location.longitude,
-    };
-    this.savedLocationArray.push(newLoc);
-    localStorage.setItem(
-      "savedLocations",
-      JSON.stringify(this.savedLocationArray)
-    );
+    if (!this.isLocationSaved) {
+      const newLoc = {
+        name: location.name,
+        admin1: location.admin1,
+        country: location.country,
+        latitude: location.latitude,
+        longitude: location.longitude,
+      };
+      this.savedLocationArray.push(newLoc);
+      localStorage.setItem(
+        "savedLocations",
+        JSON.stringify(this.savedLocationArray)
+      );
+      return `${location.name} ${location.country} saved successfully`;
+    } else {
+      return `${location.name} ${location.country} is already saved`;
+    }
   }
 
   getFavoriteLocations() {
@@ -28,5 +33,20 @@ export class LocalStorage {
     );
     if (favoriteLocations) return favoriteLocations;
     else return [];
+  }
+
+  isLocationSaved(location) {
+    const locArray = this.getFavoriteLocations();
+    locArray.forEach((loc) => {
+      if (
+        location.name === loc.name &&
+        location.admin1 === loc.admin1 &&
+        location.country === loc.country
+      ) {
+        return true;
+      }
+    });
+
+    return false;
   }
 }
